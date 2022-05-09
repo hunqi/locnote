25c07c720d41303acbe6abc125ce225b24cc5cb2
Ray <leigeosxdi@163.com>
2022-05-09 22:51:46 +0800

    test ListView

--- app/src/main/java/com/rs/locnote/FirstActivity.kt
+++ app/src/main/java/com/rs/locnote/FirstActivity.kt
@@ -1,28 +1,27 @@
 package com.rs.locnote
 
-import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.rs.locnote.databinding.FirstLayoutBinding
 
 class FirstActivity : AppCompatActivity() {
 
     private lateinit var binding: FirstLayoutBinding
+    private val data = listOf("Apple", "Banana", "Orange", "Watermelon",
+        "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango",
+        "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape",
+        "Pineapple", "Strawberry", "Cherry", "Mango")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FirstLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button1.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("https://www.baidu.com")
-            startActivity(intent)
-        }
-        supportActionBar?.hide()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, data)
+        binding.listView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
